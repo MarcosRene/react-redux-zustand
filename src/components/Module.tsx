29b67846ff3682c4bca54ecf4ab9d84351430486
Marcos Rene <renemoura789@gmail.com>
2024-04-19 20:58:58 -0300

    feat: add player slice

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -3,6 +3,8 @@ import { ChevronDown } from 'lucide-react'
 
 import { Lesson } from './Lesson'
 
+import { useAppSelector } from '../store'
+
 type ModuleProps = {
   moduleIndex: number
   title: string
@@ -10,6 +12,10 @@ type ModuleProps = {
 }
 
 export function Module(props: ModuleProps) {
+  const lessons = useAppSelector((state) => {
+    return state.player.course.modules[props.moduleIndex].lessons
+  })
+
   return (
     <Collapsible.Root className="group">
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
@@ -29,9 +35,13 @@ export function Module(props: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Fundamentos do Redux" duration="09:30" />
-          <Lesson title="Fundamentos do Redux" duration="09:30" />
-          <Lesson title="Fundamentos do Redux" duration="09:30" />
+          {lessons.map((lesson) => (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              duration={lesson.duration}
+            />
+          ))}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
